4e03c3ec065eb43077b08b4b9616598c09d37389
yixellet <yixwellet@gmail.com>
2023-11-09 21:07:26 +0400

    Как бы починил векторный слой

--- src/features/Map/Layer.jsx
+++ src/features/Map/Layer.jsx
@@ -5,34 +5,13 @@ import Vector from 'ol/layer/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
 import Select from 'ol/interaction/Select';
 import { setCN, setCNtoNull } from '../LandInfo/landInfoSlice';
-import { useGetLandsQuery } from './landsAPI';
+import { useGetActualRentedLandsQuery } from './landsAPI';
 
 export const Layer = ({ map }) => {
 
   const dispatch = useDispatch();
-  const { data } = useGetLandsQuery();
-  useEffect(() => {
-    const landsLayer = new Vector({
-      map: map,
-      zIndex: 10,
-      style: {
-        'stroke-width': 0.75,
-        'stroke-color': 'red',
-        'fill-color': 'rgba(100,100,100,0.09)',
-      },
-    })
-    
-    if (data) {
-      const vectorSource = new VectorSource({
-        features: new GeoJSON().readFeatures(data)
-      })
-      landsLayer.setSource(vectorSource)
-      landsLayer.setMap(map)
-    }
-    //console.log(data)
-    console.log(landsLayer.getSource().getFeatures())
-  })
-  /*
+  const { data } = useGetActualRentedLandsQuery();
+  
   const [landsLayer, setLandsLayer] = useState(new Vector({
     map: map,
     zIndex: 10,
@@ -45,11 +24,12 @@ export const Layer = ({ map }) => {
   
   if (data) {
     const vectorSource = new VectorSource({
-      features: new GeoJSON().readFeatures(JSON.stringify(data))
+      format: new GeoJSON(),
+      url: 'http://localhost:8080/geoserver/farm_lands/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=farm_lands%3Aactual_rented_lands&outputFormat=application%2Fjson'
     })
     landsLayer.setSource(vectorSource)
   }
-*/
+
   const featureOverlay = new Vector({
     source: new VectorSource(),
     map: map,
--- src/features/Map/landsAPI.js
+++ src/features/Map/landsAPI.js
@@ -4,13 +4,13 @@ export const landsApi = createApi({
   reducerPath: 'landsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/geoserver/farm_lands' }),
   endpoints: (builder) => ({
-    getLands: builder.query({
-      query: () => `ows?service=WFS&version=1.0.0&request=GetFeature&typeName=farm_lands%3Alands&outputFormat=application%2Fjson`,
+    getActualRentedLands: builder.query({
+      query: () => `ows?service=WFS&version=1.0.0&request=GetFeature&typeName=farm_lands%3Aactual_rented_lands&outputFormat=application%2Fjson`,
       transformResponse: (responseData) => responseData,
     }),
   }),
 });
 
 export const {
-  useGetLandsQuery
+  useGetActualRentedLandsQuery
 } = landsApi;
